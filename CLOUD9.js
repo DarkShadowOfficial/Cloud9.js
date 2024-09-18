df43950fd522e802d3048ae9cbe3138514437a76
Volatile <95543040+DarkShadowOfficial@users.noreply.github.com>
2024-09-17 21:38:17 -0500

    Update CLOUD9.js
    Fixed class naming

--- CLOUD9.js
+++ CLOUD9.js
@@ -1,7 +1,7 @@
 let distance = (obj1, obj2) => {
   return Math.sqrt((obj1.x - obj2.x) ** 2 + (obj1.y - obj2.y) ** 2);
 };
-class Game {
+class Cloud {
   constructor() {
     this.c = document.createElement("canvas");
     this.ctx = this.c.getContext("2d");
